9d03de83dc89649edf190cde7c01b8956c58433b
owenkaplan <owen.kaplan@baxter-academy.org>
2016-01-12 10:05:01 -0500

    original

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public Rigidbody2D phys;
+	public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+		phys = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+		Vector2 moveDir = new Vector2(xAxis,yAxis); 
+		phys.velocity = moveDir.normalized * speed;
+
+	}	
+}
